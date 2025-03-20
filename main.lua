7efacc230bc67fcd8734626c06073387ea1db6f4
TheWizard4166 <jamesk281@sbcglobal.net>
2025-03-20 17:49:38 -0500

    Pong lol

---
+++ main.lua
@@ -0,0 +1,63 @@
+function rectCollision(a, b)
+    return b.x <= a.x + a.width and a.x <= b.x + b.width and b.y <= a.y + a.height and a.y <= b.y + b.height
+end
+
+function circleRectCollision(circ, rect)
+    return rect.x <= circ.x + circ.r and rect.x <= circ.x + circ.r  and circ.x - circ.r <= rect.x + rect.width and rect.y <= circ.y + circ.r and circ.y - circ.r <= rect.y + rect.height
+end
+function outOfBoundsCirc(a)
+    return a.x - a.r >= wWidth and a.x + a.r <= wWidth and a.y + a.r <= wHeight and a.y - a.r >= wHeight
+end
+
+function love.load()
+    wHeight = 920 
+    wWidth = 1200
+    love.window.setMode(wWidth, wHeight)
+    speed = wHeight / 2   
+    playerRect = {
+         x = 0,
+         y = wHeight/2,
+         width = 50,
+         height = 200
+    }
+    aiRect = {
+         x = wWidth - 50,
+         y = wHeight/2,
+         width = 50,
+         height = 200
+    }
+    ball = {
+        x = wWidth/2,
+        y = wHeight/2,
+        r = 25,
+        width = 100,
+        height = 100,
+        vx = -5
+    }
+end
+
+function love.update(dt)
+    if love.keyboard.isDown("left") and playerRect.y > 0 then
+        playerRect.y = playerRect.y - speed*dt  
+    end
+    if love.keyboard.isDown("right") and playerRect.y + playerRect.height < wHeight then
+        playerRect.y = playerRect.y + speed*dt  
+    end
+    if (circleRectCollision(ball, playerRect) == false) and (circleRectCollision(ball, aiRect) == false) then
+        ball.x = ball.x + ball.vx 
+    else
+        ball.vx = ball.vx * -1
+        ball.x = ball.x + ball.vx 
+    end
+    if outOfBoundsCirc(ball) then
+        ball.x = wWidth/2
+        ball.vx = ball.vx * -1
+    end
+end
+
+function love.draw()
+    love.graphics.rectangle("fill", playerRect.x, playerRect.y, playerRect.width, playerRect.height)
+    love.graphics.rectangle("fill", aiRect.x, aiRect.y, aiRect.width, aiRect.height)
+    love.graphics.circle("fill", ball.x, ball.y, ball.r)
+end
+
